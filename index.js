0c8e2ca201b46cac68fbadb2b6017bbce930d3a6
Alessandro <alessandro.calvera@gmail.com>
2023-03-20 10:54:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const receivers = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(receivers, event) {
+    let cards = [];
+    for(let i = 0; i < receivers.length; i++){
+       cards.push(`Thank you, ${receivers[i]}, for the wonderful ${event} gift!`);
+        }
+    return cards;
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
